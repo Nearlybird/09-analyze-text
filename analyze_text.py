21acbfc7d76860dc5772d215c724f9c056220693
Nearlybird <49175582+Nearlybird@users.noreply.github.com>
2019-04-26 15:11:42 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,21 @@
 # Remember to reach out for help after your own due diligence
 
 def analyze_text(text):
-    # Your code here
+    #Create all the letters
+    alf = "abcdefghijklmnopqrstuvwxyz"
+    letters = 0
+    e = "e"
+    es = 0
+    text = text.lower()
+        for i in range(len(text)):
+            if alf.find(text[i]) >= 0:
+                letters += 1
+            else:
+                letters = letters
+            if e.find(text[i]) >= 0
+                es += 1
+            else:
+                es = es
+    #Compare each letter of WORD to letters
+        #define number of letters in WORD
+        #define number of Es in WORD
